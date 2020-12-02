b115aefe9001558ca819148b32b0741cc097eb82
Derry Bradley <derrybradley@live.ie>
2020-12-02 19:19:53 +0000

    Basic App Established with Home & About pages. Routing also implemented

---
+++ src/About.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Link} from 'react-router-dom';
+
+
+
+class About extends Component {
+
+    render() {
+
+        return (
+           <div className="about">
+               About Page!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+           </div>
+        );
+    }
+}
+
+export default About;
