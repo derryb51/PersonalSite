1a5b74b0ca53bce990ee48f4d27c9ef2467bd6b6
Derry Bradley <derrybradley@live.ie>
2020-12-05 11:18:16 +0000

    New Home Page Design With Toronto Jumbotron

--- src/Home.js
+++ src/Home.js
@@ -1,55 +1,33 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Route from 'react-router-dom/Route';
-import './App.css';
-import Header from './Header.js';
-import About from './About';
+import './Home.css';
+import Background from './assets/Toronto.jpg';
+
+var sectionStyle = {
+  width: "100%",
+  height: "1000px",
+  backgroundImage: "url(" + Background + ")"
+};
 
 class Home extends Component {
   render() {
     return (
-        <Router>
+
             <div className="app">
-                <Header/>
                 
-                <Route path="/" exact strict render={
-          () => {
-            return (
-            <div className="homePage">
-              <div className="jumboHolder">
-                 <div className="innerJumbo">
-                   </div>
+                <div id="full-size">
+                Your contents go here
+                </div>
 
-              </div>
-
-              <div className="home-tiles">
-              <div id="menu-tile">
-
-                <a href="/aboutTest"><span>About</span></a>
-              </div>
-              <div id="specials-tile">
-                <a href="/tutor">
-                  <span>Tutor</span>
-                </a>
-              </div>
-              <div id="quiz-tile">
-                <a href="/quiz">
-                <span>Quiz</span>
-                </a>
-              </div>
-              </div>
-
-            </div>);
-          }
-        }/>
-        <Route path ="/about" exact strict component={About}/>
+              
         
-
+       
             </div>
-        </Router>
+        
 
         
-    
+  
     );
   }
 }
